b8dc66a99e7dd9af71bad1523e946e33090ab55b
Godspower Idiakose <idiakeg@gmail.com>
2023-01-02 10:35:45 +0100

    Encryption using the createCipheriv

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const arr = [1, 2, 3, 4, 5];
+const fs = require("fs");
+const crypto = require("crypto");
 
 // ===REDUCE function===
 // function mul(total, value) {
@@ -13,7 +15,6 @@ const arr = [1, 2, 3, 4, 5];
 
 // ===================
 // require the crypto library
-const crypto = require("crypto");
 // Define the algorithm
 // const hash = crypto.createHash("md5");
 // // Define the key. Key is the data you want to encode
@@ -24,31 +25,56 @@ const crypto = require("crypto");
 // console.log("The generated hash code is: ", gen_hash);
 
 // ======================== ENCODING A FILE=========
-const fs = require("fs");
 
 // define the algorithm to use for encoding
-const algorithm = "md5";
-//  create the hash object and define/specify the algorithm used
-const hash = crypto.createHash(algorithm);
+// const algorithm = "md5";
+// //  create the hash object and define/specify the algorithm used
+// const hash = crypto.createHash(algorithm);
 
-// read the content of the file using streams
-const rs = fs.createReadStream("key.txt");
-// listen for the data event emitted whenever a new chunk of the file being read is available
-rs.on("data", (chunk) => {
-	// pass the chunk received to the hash object created
-	hash.update(chunk);
-});
+// // read the content of the file using streams
+// const rs = fs.createReadStream("key.txt");
+// // listen for the data event emitted whenever a new chunk of the file being read is available
+// rs.on("data", (chunk) => {
+// 	// pass the chunk received to the hash object created
+// 	hash.update(chunk);
+// });
 
 // listen for the end of the stream
-rs.on("end", () => {
-	// create a digest to access the generated hash from the chunk sent using the update(), and specify the format
-	const gen_hash = hash.digest("hex");
-	console.log(`The resulting hash is: ${gen_hash}`);
-	fs.writeFile("key.txt", gen_hash, (err) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("file writtenn successfully!!");
-		}
-	});
-});
+// rs.on("end", () => {
+// 	// create a digest to access the generated hash from the chunk sent using the update(), and specify the format
+// 	const gen_hash = hash.digest("hex");
+// 	console.log(`The resulting hash is: ${gen_hash}`);
+// 	fs.writeFile("key.txt", gen_hash, (err) => {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log("file writtenn successfully!!");
+// 		}
+// 	});
+// });
+
+const algorithm = "aes-256-cbc";
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+const bufferedKey = Buffer.from(key);
+
+function encrypt(text) {
+	// create the cipheriv method
+	const cipher = crypto.createCipheriv(algorithm, bufferedKey, iv);
+	// update the cipher with the text you want to encrypt
+	const encrypted = cipher.update(text);
+	// obtain the buffer thet contains the value of the cipher object. it is similar to digest in hashing
+	const final = cipher.final();
+	return {
+		encrypted,
+		final,
+	};
+}
+
+// sideNote: you can only run the update and final method on the created cipher method.
+
+const { encrypted, final } = encrypt("God is good");
+// the final and encrypted opbtained from the cipher final and update method call respectively are both biffers, merge both buffers together with the concat method
+let moo = Buffer.concat([encrypted, final]);
+
+console.log(moo);
