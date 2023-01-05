c63db6f4c988c69e94a686315d0d53503fb72ea9
Godspower Idiakose <idiakeg@gmail.com>
2023-01-05 15:22:34 +0100

    Alternative syntax for using middleware

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ function goo(req, res, next) {
 }
 
 // To use the middleware, register it using the app.use() method
-app.use(goo);
+// app.use(goo);
 
-app.get("/", (req, res) => {
+// Alternative to registering the middleware using app.use(), reference the middleware function in the app.get(), between the path and  the callback function
+
+app.get("/", goo, (req, res) => {
 	res.send(
 		`<h2> Welcome to the Home page</h2> <a href="/about">Go to the About page</a>`
 	);
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", goo, (req, res) => {
 	res.send("About page");
 });
 
