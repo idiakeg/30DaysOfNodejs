9f51e81530c99b2d084560426471f418d14077f6
Godspower Idiakose <idiakeg@gmail.com>
2023-01-09 11:29:15 +0100

    alternative routing syntax

--- routes/login.js
+++ routes/login.js
@@ -9,16 +9,19 @@ const {
 	deleteUser,
 } = require("../controllers/loginControllers");
 
-// post
-router.post("/", createUser);
+// // post
+// router.post("/", createUser);
 
-// get
-router.get("/", getUser);
+// // get
+// router.get("/", getUser);
 
-// put
-router.put("/:id", updateUser);
+// // put
+// router.put("/:id", updateUser);
 
-// delete
-router.delete("/:id", deleteUser);
+// // delete
+// router.delete("/:id", deleteUser);
+
+router.route("/").get(getUser).post(createUser);
+router.route("/:id").put(updateUser).delete(deleteUser);
 
 module.exports = router;
