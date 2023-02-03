4aec2e4406cccb2daaf447f513b5377057fc794a
Godspower Idiakose <idiakeg@gmail.com>
2023-02-03 13:51:09 +0100

    alternative method of connection to the database

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,13 @@
-// connection string for monogodvb atlas
+const mongoose = require("mongoose");
 
 // const connectionString =
 // 	"mongodb+srv://Godspower:711292@Gp@nodeexpressprojects.mrmqrh4.mongodb.net/?retryWrites=true&w=majority";
 
-const { MongoClient } = require("mongodb");
-
-// initialize DB
-let dbConnection;
-
 // connection string when working locally
 const connectionString = "mongodb://0.0.0.0:27017/Testing";
 
-module.exports = connectionString;
+const connectDB = () => {
+	mongoose.connect(connectionString);
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,43 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const port = 5500;
 const router = require("./routes/taskRoute");
-const { connectToDb, getDb } = require("./db/connect");
-const mongoose = require("mongoose");
-const connectionString = require("./db/connect");
+const connectDB = require("./db/connect");
 
 // connect mongoose to the database
 mongoose.set("strictQuery", false);
-mongoose
-	.connect(connectionString, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("Database connected!"))
-	.catch((err) => console.log("this is the error msg:", err));
-
-// create a schema
-const NewSchema = new mongoose.Schema({
-	name: String,
-	age: Number,
-});
 
 const app = express();
-
-// // model the created schema
-const newModel = new mongoose.model("User", NewSchema);
-
-const data = async () => {
-	const result = await newModel.create([
-		{ name: "Joshua", age: 34 },
-		{ name: "Elanga", age: 24 },
-		{ name: "Rashford", age: 15 },
-	]);
-};
-
-// call the function to insert anthony's data
-data();
 
 // middleware definitions
 app.use("/api/v1/task", router);
 
-app.listen(port, console.log(`Server is running on port: ${port}`));
+// connect to the data base
+const start = async () => {
+	try {
+		await connectDB();
+		console.log("Connected to DB!!");
+		app.listen(port, console.log(`Server is running on port: ${port}`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// start the process
+start();
