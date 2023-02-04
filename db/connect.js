37c56ab194843abc1cd4370ff76c148631e24049
Godspower Idiakose <idiakeg@gmail.com>
2023-02-04 22:28:19 +0100

    Created the .env file
    	This file is to help store secrets.

--- db/connect.js
+++ db/connect.js
@@ -4,10 +4,9 @@ const mongoose = require("mongoose");
 // 	"mongodb+srv://Godspower:711292@Gp@nodeexpressprojects.mrmqrh4.mongodb.net/?retryWrites=true&w=majority";
 
 // connection string when working locally
-const connectionString = "mongodb://0.0.0.0:27017/Testing";
 
-const connectDB = () => {
-	mongoose.connect(connectionString);
+const connectDB = (url) => {
+	mongoose.connect(url);
 };
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const port = 5500;
 const router = require("./routes/taskRoute");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // connect mongoose to the database
 mongoose.set("strictQuery", false);
@@ -15,7 +16,7 @@ app.use("/api/v1/task", router);
 // connect to the data base
 const start = async () => {
 	try {
-		await connectDB();
+		await connectDB(process.env.CONNECTION_STRING);
 		app.listen(port, console.log(`Server is running on port: ${port}`));
 	} catch (error) {
 		console.log(error);
