77929801768e685efbcb07356ae403750fb50fcb
Godspower Idiakose <idiakeg@gmail.com>
2023-02-05 08:37:17 +0100

    added validators to the schema

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,16 @@ const { Schema, model } = require("mongoose");
 
 // create the schema
 const TaskSchema = new Schema({
-	name: String,
-	completed: Boolean,
+	name: {
+		type: String,
+		trim: true,
+		required: [true, "must contain a name"],
+		maxLength: [20, "name cannot be more than 20 characters"],
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 // create the model
