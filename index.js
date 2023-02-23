f8d8e7a236dc27ae7d8c13ac4e24e77e552e1249
Godspower Idiakose <idiakeg@gmail.com>
2023-02-23 21:16:36 +0530

    Created a connection between the app and mongoDb
    	- I had to use the 121:0.0.1 in place of localhost to get rid of
    	  the error ERRONCODE / ERRORCONNECTION.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const port = process.env.PORT_NUMBER;
+const uri = process.env.CONNECTION_STRING;
+
+mongoose.set("strictQuery", false);
 
 const app = express();
 
+// middleware definitions
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(4003, () => console.log("server is running on port 4003"));
+const start = async () => {
+	try {
+		await connectDB(uri);
+		app.listen(port, () => console.log(`server is running on port: ${port}`));
+		console.log("connected to DB successfully");
+	} catch (error) {
+		console.log(`Error from mongoDB connection: ${error}`);
+	}
+};
+
+// start the app
+start();
