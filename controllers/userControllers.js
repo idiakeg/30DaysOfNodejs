b13cbabed716b82cdec080b39a0aeee7ce93a9cd
Godspower Idiakose <idiakeg@gmail.com>
2023-02-24 22:22:15 +0100

    Created the data and stored in mongoDb

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,25 @@
-const createUser = (req, res) => {
+const getHash = require("../Hmac");
+const userModel = require("../model/userSchema");
+
+const createUser = async (req, res) => {
 	const { name, email, password, phone } = req.body;
-	console.log(`name: ${name} || password: ${password}`);
-	res.send(" good doing bussiness with you");
+
+	const Password = getHash(password, phone);
+
+	// user data to be stored
+	const data = {
+		name,
+		email,
+		Password,
+		phone,
+	};
+
+	try {
+		await userModel.create(data);
+		res.redirect("/public/success.html");
+	} catch (error) {
+		res.status(500).send("An error occured");
+	}
 };
 
 module.exports = createUser;
