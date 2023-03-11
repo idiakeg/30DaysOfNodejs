b970bd520fa049b7ea836d05743931428f99052f
Godspower Idiakose <idiakeg@gmail.com>
2023-03-11 14:35:03 +0100

    Basic Web Socket connections
    	- Here, a new web socket server was initialized and we have
    	  access to it via the wss variable
    
    	- The connection event is emitted when a connection has been
    	  created with the server(wss)
    
    	- On the wss servere, we can now listen for open, message, error
    	  and close events
    
    	- In this block of code, we just console.log to show that said
    	  events have been emitted
    
    	- open event as stated above here, doesnot actually exist on the
    	  server side, it exist on the client side, it is similar to the
    	  connection event

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.get("/admin", (req, res) => {
 	res.sendFile(path.join(__dirname, "static/admin.html"));
 });
 
+// =======Web Socket =======
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+	console.log("mew client connected");
+	ws.on("message", (data) => {
+		console.log(data);
+	});
+	ws.on("close", () => {
+		console.log("client has disconnected");
+	});
+	ws.on("error", (error) => {
+		console.log("Something went wrong");
+	});
+});
+
 server.listen(port, () => {
 	console.log(`server is running on port: ${port}`);
 });
