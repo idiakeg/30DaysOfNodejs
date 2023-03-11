c35d2def924b68145be03eb961276b508fe48f16
Godspower Idiakose <idiakeg@gmail.com>
2023-03-11 14:28:38 +0100

    Setup basic routing
    	- Here, the files present in the static folder are being exposed
    	  to users when they visit the end point.
    
    	- However, when an admin visit the end point for admins, a
    	  different pages is offered the person(admin.html)

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const port = process.env.PORT;
 
 const server = http.createServer(app);
 
+// middleware definitons
+app.use(express.static("static"));
+
+// basic routes
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "static/index.html"));
+});
+
+app.get("/admin", (req, res) => {
+	res.sendFile(path.join(__dirname, "static/admin.html"));
+});
+
 server.listen(port, () => {
 	console.log(`server is running on port: ${port}`);
 });
