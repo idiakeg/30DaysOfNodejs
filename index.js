e905e54a2bc8472d2a806b2ba419e499d741f977
Godspower Idiakose <idiakeg@gmail.com>
2023-03-11 14:21:40 +0100

    created a server with express and listened

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const app = express();
 require("dotenv").config();
 const path = require("path");
 const port = process.env.PORT;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+	console.log(`server is running on port: ${port}`);
+});
