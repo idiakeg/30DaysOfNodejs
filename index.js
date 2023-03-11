2615fd57fc67433ed8c771e0dfff33a849f6623d
Godspower Idiakose <idiakeg@gmail.com>
2023-03-11 13:19:58 +0100

    installed required modules

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const app = express();
-const routes = require("./routes/login");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// register the routes to take effect whenever the base url "/login" is visited / requested
-app.use("/login", routes);
-
-app.get("/", (req, res) => {
-	res.send("Welcome!! Go to the /login route to access users");
-});
-
-app.listen(5500, () => console.log("server is running on port 5500"));
+require("dotenv").config();
+const path = require("path");
+const port = process.env.PORT;
