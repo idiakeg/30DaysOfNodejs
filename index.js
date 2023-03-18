428e4c0d393fdc199fb9272ff6b088baa14d03eb
Godspower Idiakose <idiakeg@gmail.com>
2023-03-18 21:27:03 +0100

    basic websocket server setup

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const WebSocket = require("ws");
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = 6061;
+const path = require("path");
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+	console.log("Client connected to the server");
+	ws.onmessage = () => {
+		console.log("message received");
+	};
+	ws.onclose = () => {
+		console.log("client disconnected");
+	};
+});
+
+server.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
